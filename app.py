14758c791dccc3c975f65cf74e488f464c6071af
Shubham2209-Mits <shubham27071997@gmail.com>
2023-08-04 15:55:42 +0530

    data base connection

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+   
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect('mydatabase.sqlite')
+    cursor = conn.cursor()
+
+    # Insert data into the database
+    cursor.execute('INSERT INTO form_data (name, email ,message) VALUES (?, ?, ?)', (name, email, message ))
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
+
+    return 'Data submitted successfully!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
